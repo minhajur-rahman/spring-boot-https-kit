c1b68340c7e1b5c82610b2d07da1ee3459a72fba
jack <sychewo@gmail.com>
2014-06-06 11:15:58 +0800

    add httpsEnabled flag

--- src/main/java/org/ycavatars/sboot/kit/TomcatConfig.java
+++ src/main/java/org/ycavatars/sboot/kit/TomcatConfig.java
@@ -25,38 +25,55 @@ public class TomcatConfig {
   @Bean(name = "tomcatCustomizer")
   public EmbeddedServletContainerCustomizer containerCustomizer(
       @Value("${connector.https.enabled") Boolean httpsEnabled,
-      @Value("${connector.https.keystoreFile}") Resource keystoreFile,
-      @Value("${connector.https.keystorePass}") String keystorePass,
-      @Value("${connector.https.keyAlias") String keyAlias) {
+      @Value("${connector.https.keystoreFile}") final Resource keystoreFile,
+      @Value("${connector.https.keystorePass}") final String keystorePass,
+      @Value("${connector.https.keyAlias") final String keyAlias) {
 
-    //refer to:
-    //http://docs.spring.io/spring-boot/docs/1.0.2.RELEASE/reference/htmlsingle/#howto-terminate-ssl-in-tomcat
-    TomcatConnectorCustomizer sslConnectorCustomizer = (Connector connector) -> {
-      connector.setSecure(true);
-      connector.setScheme("https");
-      connector.setAttribute("keyAlias", keyAlias);
-      connector.setAttribute("keystorePass", keystorePass);
-      try {
-        connector.setAttribute("keystoreFile", keystoreFile.getFile().getAbsolutePath());
-      } catch (IOException e) {
-        logger.error("cannot load keystore", e);
-        throw new IllegalStateException("Cannot load keystore", e);
+    if (!httpsEnabled) {
+      return new EmbeddedServletContainerCustomizer() {
+        @Override public void customize(ConfigurableEmbeddedServletContainer container) {
+          //do nothing
+        }
+      };
+    }
+
+    EmbeddedServletContainerCustomizer tomcatCustomizer = new EmbeddedServletContainerCustomizer() {
+      @Override public void customize(ConfigurableEmbeddedServletContainer container) {
+        if (container instanceof TomcatEmbeddedServletContainerFactory) {
+          TomcatEmbeddedServletContainerFactory tomcatFactory =
+              (TomcatEmbeddedServletContainerFactory) container;
+          tomcatFactory.addConnectorCustomizers(
+              sslConnectorCustomizer(keystoreFile, keystorePass, keyAlias));
+        }
       }
-      connector.setAttribute("clientAuth", "false");
-      connector.setAttribute("sslProtocol", "TLS");
-      connector.setAttribute("SSLEnabled", true);
     };
-
-    EmbeddedServletContainerCustomizer tomcatCustomizer =
-        (ConfigurableEmbeddedServletContainer containerFactory) -> {
-          if (containerFactory instanceof TomcatEmbeddedServletContainerFactory) {
-            TomcatEmbeddedServletContainerFactory tomcatFactory =
-                (TomcatEmbeddedServletContainerFactory) containerFactory;
-            tomcatFactory.addConnectorCustomizers(sslConnectorCustomizer);
-          }
-        };
 
     return tomcatCustomizer;
   }
 
+  protected TomcatConnectorCustomizer sslConnectorCustomizer(
+      final Resource keystoreFile,
+      final String keystorePass,
+      final String keyAlias) {
+
+    //refer to:
+    //http://docs.spring.io/spring-boot/docs/1.0.2.RELEASE/reference/htmlsingle/#howto-terminate-ssl-in-tomcat
+    return new TomcatConnectorCustomizer() {
+      @Override public void customize(Connector connector) {
+        connector.setSecure(true);
+        connector.setScheme("https");
+        connector.setAttribute("keyAlias", keyAlias);
+        connector.setAttribute("keystorePass", keystorePass);
+        try {
+          connector.setAttribute("keystoreFile", keystoreFile.getFile().getAbsolutePath());
+        } catch (IOException e) {
+          logger.error("cannot load keystore", e);
+          throw new IllegalStateException("Cannot load keystore", e);
+        }
+        connector.setAttribute("clientAuth", "false");
+        connector.setAttribute("sslProtocol", "TLS");
+        connector.setAttribute("SSLEnabled", true);
+      }
+    };
+  }
 }
