4e51d18d2d4c45a5a0c139b178285de1f75165f2
jack <sychewo@gmail.com>
2014-06-09 10:45:28 +0800

    ignore p12 keystore

---
+++ src/test/java/org/ycavatars/sboot/kit/TomcatConfigTest.java
@@ -0,0 +1,15 @@
+package org.ycavatars.sboot.kit;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration
+public class TomcatConfigTest {
+
+
+  public testContainerCustomizer_enableHttps() {
+
+  }
+}
---
+++ src/test/java/org/ycavatars/sboot/kit/mock/App.java
@@ -0,0 +1,7 @@
+package org.ycavatars.sboot.kit.mock;
+
+/**
+ * Created by jack on 6/6/14.
+ */
+public class App {
+}
